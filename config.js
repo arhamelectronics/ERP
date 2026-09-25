28469079e7d0e33cac00b6a439dc5bd850f8988c
arhamelectronics <syedessafp@gmail.com>
2026-09-26 01:26:17 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.ERP_CONFIG={
+  API_URL:"https://script.google.com/macros/s/AKfycbyi8CaMtMxV7Prf5Dexoy03ao8v2XApxbw2rLK2hTlvYS_j9vV3Y7JbW-GrAS3XYUvAtA/exec",
+  APP_NAME:"Arham Electronics ERP",
+  CURRENCY:"PKR"
+};
